590a31036ac75ce92bf506d5ff282eeadf67eb4f
Amir Azizafshari <a_amirhossein@icloud.com>
2020-07-28 10:49:48 -0400

    Added library classes for ANCAviationLib based on UML. Seperated them into 4 different namespaces.

---
+++ ANCAviationLib/Fetcher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ANCAviationLib
+{
+    abstract class Fetcher
+    {
+    }
+}
--- FINAL_PROJECT_GROUP4/MainPage.xaml.cs
+++ FINAL_PROJECT_GROUP4/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ANCAviationLib.COVID;
+using ANCAviationLib;
+using ANCAviationLib.Flights;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,7 @@ namespace FINAL_PROJECT_GROUP4
         public MainPage()
         {
             this.InitializeComponent();
+            
         }
     }
 }
