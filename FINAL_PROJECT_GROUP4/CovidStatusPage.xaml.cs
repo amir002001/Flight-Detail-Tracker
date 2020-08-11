3c858c5a16105a76413d27ce5040fff2b4a30445
Navya <Navya@DESKTOP-MVNSCDL>
2020-08-11 17:34:49 -0400

    Created UI

--- FINAL_PROJECT_GROUP4/CovidStatusPage.xaml.cs
+++ FINAL_PROJECT_GROUP4/CovidStatusPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ANCAviationLib.COVID;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,4 +29,14 @@ namespace FINAL_PROJECT_GROUP4
             this.InitializeComponent();
         }
     }
+   // private async void SaveOnClick(object sender, RoutedEventArgs e)
+   //    {
+           // FileSavePicker savePicker = new FileSavePicker();
+//           savePicker.SuggestedStartLocation = PickerLocationId.Desktop;
+//           savePicker.FileTypeChoices.Add("Json", new List<String> { ".json" });
+//           savePicker.SuggestedFileName = "SavedFlight.json";
+//           StorageFile storageFile = await savePicker.PickSaveFileAsync();
+//
+//         JsonSaver.Save<FlightDetails>(await storageFile.OpenStreamForWriteAsync(), _selectedFlight);
+     //  }
 }
